c230d21445db49b8efd293774b3c903849069496
Frank Alimimian <alimimianfrank@gmail.com>
2023-04-13 17:36:52 +0100

    Fix functionality This commit updates the display of book read status based on the state of the isRead checkbox.

--- script.js
+++ script.js
@@ -1,126 +1,125 @@
-'strict mode'
+"strict mode";
 //global variables
-let myLibrary =[];
-let newBook;
+let myLibrary = [];
 
 
 //cache the dom
-const addBookBtn = document.getElementById('add-book-btn');
-const modalContainer = document.getElementById('modal-container');
-const isReadToggle = document.getElementById('is-read-toggle');
-const bookForm = document.getElementById('book-form');
-modalContainer.style.display = 'none';
-
-
+const addBookBtn = document.getElementById("add-book-btn");
+const modalContainer = document.getElementById("modal-container");
+let readStatus = document.getElementById("read-status");
+const bookForm = document.getElementById("book-form");
+modalContainer.style.display = "none";
+const errorMssg = document.getElementById("erorr-mssg");
 
 
 //The constructor that will get the user info and store them in myLibrary array.
 
- 
-const Book = function(author, tittle, pages){
-  this.author = author,
-  this.tittle= tittle,
-  this.pages = pages;
-  this.read = this.false;
+const Book = function (author, tittle, pages, isRead) {
+  (this.author = author),
+    (this.tittle = tittle),
+    (this.pages = pages),
+    (this.isRead = isRead);
+};
+
+Book.prototype.displayConsole = () => {
+  console.log("this is a bookprototype");
+};
+addBookBtn.addEventListener("click", () => {
+  modalContainer.style.display = "block";
+  addBookBtn.style.visibility = "hidden";
   
- };
- 
-  addBookBtn.addEventListener('click', ()=>{
-    modalContainer.style.display = 'block';
-    addBookBtn.style.visibility ='hidden';
-   Book.prototype.displayConsole = () =>{
-    console.log('this is a bookprototype')
-   }
-   
-  });
-const ResetUserInput = function(){
-  document.getElementById('author').value = '';
-  document.getElementById('tittle').value = '';
-  document.getElementById('pages').value = '';
-}
+});
 
-const displayBook = function (){
- const bookContainer = document.getElementById('book-container');
-  bookContainer.innerHTML ='';
-  ;
+const ResetUserInput = function () {
+  document.getElementById("author").value = "";
+  document.getElementById("tittle").value = "";
+  document.getElementById("pages").value = "";
+  errorMssg.textContent = "";
+  errorMssg.style.display = "none";
+  readStatus.checked = false;
+};
+
+const displayBook = function () {
+  const bookContainer = document.getElementById("book-container");
+  bookContainer.innerHTML = "";
   // to loop through the array and create Html element for each book object.
-  myLibrary.forEach((book) =>{
+  myLibrary.forEach((book) => {
+    const bookItem = document.createElement("div");
+    const bookContainerHeader = document.createElement("h3");
+    bookContainerHeader.classList.add("book-container-header");
+    bookContainerHeader.textContent = "Book";
 
-   const  bookItem =document.createElement('div');
-    const bookContainerHeader = document.createElement('h3');
-    bookContainerHeader.classList.add('book-container-header');
-    bookContainerHeader.textContent ="Book";
-    
     bookItem.appendChild(bookContainerHeader);
 
-
-    bookItem.classList.add('book-item');
-    const author = document.createElement('p');
-    author.textContent=`Author: ${book.author}`;
+    bookItem.classList.add("book-item");
+    const author = document.createElement("p");
+    author.textContent = `Author: ${book.author}`;
     bookItem.appendChild(author);
 
-    const tittle = document.createElement('p');
+    const tittle = document.createElement("p");
     tittle.textContent = `Tittle: ${book.tittle}`;
     bookItem.appendChild(tittle);
 
-    const pages = document.createElement('p');
-    pages.textContent = `Pages: ${book.pages}`
+    const pages = document.createElement("p");
+    pages.textContent = `Pages: ${book.pages}`;
     bookItem.appendChild(pages);
+    console.log(book);
+
+    const readCheck = document.createElement("p");
+
    
+    readCheck.textContent = book.isRead ? "Read" : "Not Read";
+    console.log(readCheck.textContent);
 
-    bookContainer.append(bookItem)
-    });
+    bookItem.appendChild(readCheck);
+
+    bookContainer.append(bookItem);
     
-}
+  });
+};
 
- 
-const addBookToLibrary = function(){
- 
-  bookForm.addEventListener('submit', (event) => event.preventDefault())
-  const authorValue = document.getElementById('author').value;
-const tittleValue = document.getElementById('tittle').value;
-const pagesValue = document.getElementById('pages').value;
+const addBookToLibrary = function () {
+  const form = document.querySelector("form");
 
-
-
-  let newBook = new Book(authorValue,tittleValue,pagesValue);
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    // Perform form validation and submission logic here
+  });
+  bookForm.addEventListener("submit", (event) => event.preventDefault());
+  const authorValue = document.getElementById("author").value;
+  const tittleValue = document.getElementById("tittle").value;
+  const pagesValue = document.getElementById("pages").value;
   
-    function validateInput(){
-    if (authorValue.trim() !==''){
+
+  let newBook = new Book(
+    authorValue,
+    tittleValue,
+    pagesValue,
+    readStatus.checked        
+  );
+
+  function validateInput() {
+    if (
+      authorValue.trim() !== "" &&
+      tittleValue.trim() !== "" &&
+      pagesValue.trim() !== ""
+    ) {
       myLibrary.push(newBook);
+
+      displayBook();
+      ResetUserInput();
+      addBookBtn.style.visibility = "visible";
+     
+      modalContainer.style.display = "none";
+    } else {
+      errorMssg.textContent = "please complete form";
+      errorMssg.style.display = "block";
     }
-    else if (tittleValue.trim() !==""){
-      myLibrary.push(newBook);
-    }
-    else if (pagesValue.trim() !== ""){
-      myLibrary.push(newBook)
-    }
-    else{
-      if(authorValue == ''){
-        errorMssg
-      }
-      else if(tittleValue == ''){
-        errorMssg
-      }
-      else if(pagesValue == "" ){
-        errorMssg
-      }
-    }
-    
   }
- 
- newBook.displayConsole();
-  
-   
-   validateInput();
-   displayBook();
-   
-  addBookBtn.style.visibility = 'visible';
- ResetUserInput();  
-  modalContainer.style.display ='none'
-   return myLibrary, newBook;
+  newBook.displayConsole();
+  validateInput();
+};
 
-}
-
-document.querySelector('#submit-btn').addEventListener('click', addBookToLibrary,);
-
+document
+  .querySelector("#submit-btn")
+  .addEventListener("click", addBookToLibrary);
